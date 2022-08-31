1489c2a1a28aff57542669cc347bf83c85156722
lewismati <lewis.mati@student.moringaschool.com>
2022-09-01 02:48:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-const sinon = require( 'sinon' )
-
-describe('reducer', function() {
-  describe('batteries', function() {
-    it('should have a `totalBatteries` variable', function() {
-      expect(totalBatteries).to.exist;
-    });
-
-    it('should have a number as a result', function() {
-      expect(totalBatteries).to.be.a('number');
-    });
-
-    it('should have made the sum of all the assembled batteries', function() {
-      expect(totalBatteries).to.eql(31);
-    });
-  });
-});
+const totalBatteries = batteryBatches.reduce(function (total, value) {
+    return total => value;
+}, 0)
